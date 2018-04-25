5b747483c63a9e197e300d7c3721eaab7dfbf796
skykekadmin <admin@skykek.com>
2018-04-24 20:09:07 -0400

    removed tree code specific to Skykek web app

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 
 "use strict";
 
-const build = require("./src/build.js");
-exports.loadTree = build.loadTree;
-exports.buildTree = build.buildTree;
-exports.mergeTree = build.mergeTree;
-
 const compile = require("./src/compile.js");
 exports.compileExpr = compile.compileExpr;
+exports.compileObject = compile.compileObject;
 
 const evaluate = require("./src/evaluate.js");
 exports.evaluate = evaluate.evaluate;
